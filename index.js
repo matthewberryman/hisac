516a9a2051292cfcf986be39c5a0a4a974c6b28e
Matthew Berryman <matthew@acrossthecloud.net>
2021-02-21 09:55:24 +1100

    Rusting armour for effect

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const bent = require('bent'); // get bent
 
 const rl = readline.createInterface({
-  input: fs.createReadStream('test.txt')
+  input: fs.createReadStream('guesses.txt')
 });
 
 const post = bent('https://api.hisac.computer/', 'POST', 'json', 200);
 
 rl.on('line', async (line) => {
   word = line.replace(/[\r\n]+$/g,''); // strip trailing new line chars
-  const response = await post('command', {'func':'open','directory':'/','args':['EXTERNAL','-k',word]});
+  const response = await post('command', {'func':'open','directory':'ARCHIVES/','args':['FREEBIRD','-k',word]});
   if ('content' in response) // bingo
   {
     console.log('key:',word,'\ncontent:',response.content);
