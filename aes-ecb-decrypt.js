db132c6116e8337e2ac1d86fbd9432ce9b75a4bc
Matthew Berryman <matthew@acrossthecloud.net>
2021-02-21 10:54:08 +1100

    a little more work

--- aes-ecb-decrypt.js
+++ aes-ecb-decrypt.js
@@ -3,15 +3,18 @@ const SWIFT = 'oHSm3Vhwjz0L+LyMssgQNEwBrwxVS8dD/AmLQw165LaFYpdidXDnllSWGetFs5hAG
 const ciphertext = Buffer.from(SWIFT, 'base64').toString('hex');
 
 const crypto = require('crypto'),
-    algorithm = 'aes-128-ecb'; // ECB hint from ls
+    algorithm = 'aes-128-ecb'; // ECB hint from ls. HISAC only specifies mode not algo though
 
 
 const decrypt = (text,password) => {
+
   var decipher = crypto.createDecipheriv(algorithm,password,null); // ECB has no iv so set to null
   var dec = decipher.update(text,'hex','utf8')
   dec += decipher.final('utf8');
   return dec;
 }
-console.log(ciphertext.length); // should be multiple of 16
+console.log(ciphertext.length); // should be multiple of 16x2 (x2 because hex so two chars per byte)
  
 //console.log(decrypt(ciphertext,'Fe2O3')); // key should also be 16 bytes - currently generates error
+
+console.log(decrypt(ciphertext,'Fe2O3Fe2O3Fe2O3F')); // pad by repetition, still generates error?
