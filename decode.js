0149cea9a6ba218e2b86645914af1ac38d6d436c
Matthew Berryman <matthew@acrossthecloud.net>
2021-02-23 22:27:15 +1100

    twitter fail bird

---
+++ decode.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const fs = require('fs');
+const bent = require('bent'); // get bent
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('guesses.txt')
+});
+
+const post = bent('https://api.hisac.computer/', 'POST', 'json', 200);
+
+main = async () => {
+  const SWIFT = (await post('command', {'func':'list','directory':'ARCHIVES/','args':['SWIFT']})).content;
+  const methodGuesses = ['list','decode','base64decode','ECB','deciper','decrypt','open','unlock'];
+  const keyGuesses = ['Fe2O3', 'ironoxide', 'this', 'candle'];
+  const payloadGuesses = [SWIFT, 'SWIFT'];
+  methodGuesses.forEach(async (method) => {
+    keyGuesses.forEach(async (key) => {
+      payloadGuesses.forEach(async (payload) => {
+        // console.log('trying',method,key,payload);
+        const response = await post('command', {'func':'freebird','directory':'ARCHIVES/','args':[method,key,payload]});
+        console.log(response); // always { error: 'freebird [method] [key] [payload]' }  
+      });
+    });
+  });
+}
+
+main();
