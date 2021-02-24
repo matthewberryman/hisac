1708e45b2b01eff3321aa408657e8449dbeea28d
Matthew Berryman <matthew@acrossthecloud.net>
2021-02-25 09:52:33 +1100

    bug fix and more guesses

--- decode.js
+++ decode.js
@@ -16,16 +16,16 @@ const post = bent('https://api.hisac.computer/', 'POST', 'json', 200);
 
 main = async () => {
   const SWIFT = (await post('command', {'func':'list','directory':'ARCHIVES/','args':['SWIFT']})).content;
-  const methodGuesses = ['list','decode','base64decode','ECB','deciper','decrypt', 'decryption', 'open','unlock','B64'];
-  const keyGuesses = ['Fe2O3', 'ironoxide', 'this', 'candle'];
+  const methodGuesses = ['list','decode','base64decode','ECB','deciper','decrypt', 'decryption', 'open','unlock','B64','become','looking-glass','portal'];
+  const keyGuesses = ['Fe2O3', 'ironoxide', 'this', 'candle','Jubjub'];
   const payloadGuesses = [SWIFT, 'SWIFT'];
   methodGuesses.forEach(async (method) => {
     keyGuesses.forEach(async (key) => {
       payloadGuesses.forEach(async (payload) => {
         // console.log('trying',method,key,payload);
-        const response = await post('command', {'func':'freebird','directory':'ARCHIVES/','args':sb([method,key,payload])});
+        let response = await post('command', {'func':'freebird','directory':'ARCHIVES/','args':sb([method,key,payload])});
         console.log(response); // always { error: 'freebird [method] [key] [payload]' }  
-        const response = await post('command', {'func':'freebird','directory':'ARCHIVES/','args':[method,key,payload]});
+        response = await post('command', {'func':'freebird','directory':'ARCHIVES/','args':[method,key,payload]});
         console.log(response); // always { error: 'freebird [method] [key] [payload]' }  
       });
     });
